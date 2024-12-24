b87d3aae00afc9805eb2691f6fc6c8912433573b
Mohamad Maraqa <mohamadmaraqa@Mohamads-MacBook-Pro.local>
2024-12-24 10:01:13 +0200

    asd

--- resources/views/web/product/index.blade.php
+++ resources/views/web/product/index.blade.php
@@ -3,20 +3,32 @@
 <form class="row" method="post" action="{{ route('cart.addToCart') }}">
     @csrf
     <input type="hidden" name="product_id" value="{{ $data->id }}">
-    <div class="col-md-12">
-        <h4>{{ $data->product_name }}</h4>
-    </div>
+
+    <div class="col-md-6">
+<div class="row">
     <div class="col-md-12">
         <img style="max-height: 500px;min-height: 500px" src="{{ asset('storage/product/'.$data->product_image)}}" class="card-img-top" alt="...">
     </div>
-    <div class="col-md-12">
-        <p>{{ $data->product_description }}</p>
+
+</div>
     </div>
-    <div class="col-md-12">
-        <p><span>₪</span><span>{{ $data->product_price }}</span></p>
-    </div>
-    <div class="col-md-12">
-        <button class="btn btn-dark">اضافة للسلة</button>
+    <div class="col-md-6">
+        <div class="row">
+            <div class="col-md-12">
+                <h4>{{ $data->product_name }}</h4>
+            </div>
+            <div class="col-md-12">
+                <p>{{ $data->product_description }}</p>
+            </div>
+            <div class="col-md-12">
+                <p><span>₪</span><span>{{ $data->product_price }}</span></p>
+            </div>
+            <div class="col-md-12">
+                <button class="btn btn-dark">اضافة للسلة</button>
+            </div>
+
+        </div>
+
     </div>
 </form>
 
